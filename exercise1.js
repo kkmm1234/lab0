598f544fa8e923b806de9a20819c4131bbb2f477
kkmm1234 <keithmoffitt2001@gmail.com>
2023-09-22 11:08:22 +0000

    first

---
+++ exercise1.js
@@ -0,0 +1,25 @@
+console.log("Hello World");
+
+function SayHello(){
+    console.log("Hello");
+}
+
+SayHello();
+
+const myFunction = function sayHi(){
+    console.log("Hi");
+}
+
+myFunction();
+
+const myNewFunction = function(){
+    console.log("Bonjour!");
+}
+
+myNewFunction();
+
+const myComponent = ()=>{
+    console.log("Bye");
+}
+
+myComponent();
